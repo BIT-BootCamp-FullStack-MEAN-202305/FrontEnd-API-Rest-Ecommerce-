f4236645d771eb053d5ee13c13af324d0a5b143f
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-07-12 20:56:57 -0500

    [+] saludo.component: Crea un componente de forma manual

---
+++ src/app/components/saludo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'como-quiero',    // app-saludo
+    templateUrl: './saludo.component.html',
+    styleUrls: [ './saludo.component.css' ]
+})
+export class Saludo {
+    saludo = 'Hola Mundo desde Angular';
+}          // SaludoComponent
