fefed96cb61fadfa1c083fcae0bffa4f00284ef4
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-07-17 21:21:09 -0500

    [+] ng g m protected --routing [+] ng g c protected/pages/dashboard [+] app-routing.module: Definir ruta de modulo 'dashboard' [+] protected-routing.module: Definir rutas hijas del modulo 'dashboard'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,11 @@ const routes: Routes = [                              // -> Path        Page
   { path: '', component: HomeComponent },             // -> /           Home Page     http://localhost:4200/
   { path: 'login', component: LoginComponent },       // -> /login      Login Page    http://localhost:4200/login
   { path: 'register', component: RegisterComponent }, // -> /register   Register Page http://localhost:4200/register
+  { // Define ruta de modulo usando LazyLoad (Carga Perezosa)
+    path: 'dashboard',
+    loadChildren: () => import( './protected/protected.module' )
+                            .then( ( module ) => module.ProtectedModule )
+  },
   { path: '**', redirectTo: ''  }                     // -> /hola       Home Page     http://localhost:4200/
 ];
 
