17d38b9a20e67916a60058a9f343d054107dc33f
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-07-17 20:38:30 -0500

    [+] register.component: Implementamos Formularios Reactivos (Validacion)

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  // Atributos
+  registerForm: FormGroup = this.fb.group({
+    nombre: [
+      '',   // Valor por defecto del campo
+      [     // Validaciones para campo
+        Validators.required
+      ]
+    ],
+    email: [
+      '',   // Valor por defecto para el campo
+      [     // Validaciones para el campo
+        Validators.required,
+        Validators.email
+      ]
+    ],
+    password: [
+      '',   // Valor por defecto para el campo
+      [     // Validaciones para el campo 
+        Validators.required,
+        Validators.minLength( 8 ),
+      ]
+    ]
+  });
 
+  // Constructor: Inyeccion de dependencias
+  constructor( private fb: FormBuilder ) {}
+
+  // Metodo: Register
+  register() {
+    console.group( 'registerForm' );
+    console.info( this.registerForm.value );
+    console.info( this.registerForm.valid );
+    console.groupEnd();
+  }
 }
