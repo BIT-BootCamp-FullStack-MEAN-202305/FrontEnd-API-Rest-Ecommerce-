541c1a2ccc50b5835f4345ff047be4f1cd9ad526
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-07-17 20:07:54 -0500

    [+] app.module: Importamos ReactiveFormsModule [+] login.component: Implementamos Formularios Reactivos (Validacion)

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  // Atributos de la clase
+  loginForm: FormGroup = this.fb.group({
+    email: [ 
+      '',   // Valor predeterminado del campo
+      [     // Validaciones
+        Validators.required,
+        Validators.email
+      ] 
+    ],
+    password: [
+      '',   // Valor predeterminado del campo
+      [     // Validaciones
+        Validators.required,
+        Validators.minLength( 8 )
+      ]
+    ]
+  });
+
+  /** Constructor: Inyeccion de dependencias */
+  constructor( private fb: FormBuilder ) {}
+
+  /** Metodo: Login */
+  login() {
+    console.group( 'loginForm' );
+    console.info( this.loginForm.value );
+    console.info( this.loginForm.valid );
+    console.groupEnd();
+  }
 
 }
